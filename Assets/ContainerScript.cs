2eeb7f8315237bc71fbd5b39ca67f9fcfb0fa3c6
Maciej Piekarz <mac.eek@gmail.com>
2023-02-18 17:06:37 +0100

    initial commit

---
+++ Assets/ContainerScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class ContainerScript : MonoBehaviour
+{
+    public AssetReference addressableIn;
+    public AssetReference addressableOut;
+}
---
+++ Assets/MainScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class MainScript : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        yield return Addressables.LoadAssetsAsync<GameObject>("Foo", (asset) => { });
+
+        var container = Addressables.LoadAssetAsync<GameObject>("Container.prefab");
+        var inFolderWithAddress = Addressables.LoadAssetAsync<GameObject>("AddressableFolder/AddressableInFolder.prefab");
+        var outsideFolderWithAddress = Addressables.LoadAssetAsync<GameObject>("AddressableOutside.prefab");
+
+        var inFolderWithKey = Addressables.LoadAssetAsync<GameObject>(container.Result.GetComponent<ContainerScript>().addressableIn.RuntimeKey);
+        var outsideFolderWithKey = Addressables.LoadAssetAsync<GameObject>(container.Result.GetComponent<ContainerScript>().addressableOut.RuntimeKey);
+
+        Debug.Log(container.Status);
+        Debug.Log(inFolderWithAddress.Status);
+        Debug.Log(outsideFolderWithAddress.Status);
+
+        Debug.Log(inFolderWithKey.Status);
+        Debug.Log(outsideFolderWithKey.Status);
+    }
+}
